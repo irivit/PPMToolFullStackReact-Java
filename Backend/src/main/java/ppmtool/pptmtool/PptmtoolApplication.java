2f7f489a1c3379b03d2d352969bd38049cf56f2b
irivi <copacies@gmail.com>
2019-04-15 20:53:34 -0500

    backend inside

---
+++ Backend/src/main/java/ppmtool/pptmtool/PptmtoolApplication.java
@@ -0,0 +1,13 @@
+package ppmtool.pptmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PptmtoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PptmtoolApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/ppmtool/pptmtool/PptmtoolApplicationTests.java
@@ -0,0 +1,16 @@
+package ppmtool.pptmtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PptmtoolApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
